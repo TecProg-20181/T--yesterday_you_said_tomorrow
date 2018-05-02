51a17a62e5b86dfd9f429c99edee8489bc8dc935
filipetoyoshima <filipetoyoshima@gmail.com>
2018-05-02 13:40:25 -0300

    Created file to comport all constants
    Co-authored-by: cjjcastro <cjj.castro3@gmail.com>

---
+++ constants.py
@@ -0,0 +1,33 @@
+"""
+    File containing constants useds in the rest of
+    the program
+"""
+import os
+
+TOKEN = os.environ['BOT_API_TOKEN']
+URL = "https://api.telegram.org/bot{}/".format(TOKEN)
+
+TODO = 'TODO'
+DOING = 'DOING'
+DONE = 'DONE'
+HELP = """
+ /new NOME
+ /todo ID
+ /doing ID
+ /done ID
+ /delete ID
+ /list{I (list by id), P (list by priority)}
+ /rename ID NOME
+ /dependson ID ID...
+ /duplicate ID
+ /priority ID PRIORITY{low, medium, high}
+ /duedate ID DATE{YYYY-MM-DD}
+ /help
+"""
+
+# ICONS
+NEW_ICON = '\U0001F195'
+DOING_ICON = '\U000023FA'
+DONE_ICON = '\U00002611'
+TASK_ICON = '\U0001F4CB'
+STATUS_ICON = '\U0001F4DD'
