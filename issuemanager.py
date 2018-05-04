0407dad730f165a69050b294ef4c3becf347f554
Hiroshi18 <lucas.hh@hotmail.com>
2018-05-04 02:22:56 -0300

    Create file for issue manager

--- issuemanager.py
+++ issuemanager.py
@@ -1,11 +1,11 @@
 import requests
 import constants
-from BotManager import BotManager
+from urlhandler import UrlHandler
 
 
 class IssueManager:
     def __init__(self):
-        self.bot_manager = BotManager()
+        self.url_handler = UrlHandler()
 
     def split_message(self, msg):
         text = ''
@@ -30,14 +30,14 @@ class IssueManager:
 
         print(response.text)
 
-        return self.bot_manager.send_message("New Issue created {}".format(name), chat)
+        return self.url_handler.send_message("New Issue created {}".format(name), chat)
 
     def rename_issue(self, msg, chat):
         """rename a task by id"""
         msg, text = self.split_message(msg)
 
         if text == '':
-            self.bot_manager.send_message("""
+            self.url_handler.send_message("""
                           You want to modify the issue {},
                           but you didn't provide any new text
                          """.format(msg), chat)
@@ -56,7 +56,17 @@ class IssueManager:
         try:
             result['message']
         except:
-            self.bot_manager.send_message("Issue does not exist", chat)
+            self.url_handler.send_message("Issue does not exist", chat)
 
         requests.request("POST", constants.URL_GITHUB+'/'+msg, data=payload, headers=headers)
-        return self.bot_manager.send_message("Issue renamed {}".format(text), chat)
+        return self.url_handler.send_message("Issue renamed {}".format(text), chat)
+
+    def list_issues(self, chat):
+        """lists all the issues active in the T--yesterday_you_said_tomorrow repo"""
+        issues = self.url_handler.get_json_from_url(constants.URL_GITHUB)
+        msg = ''
+        msg += '\U0001F4CB Issues List\n\n'
+        for aux in issues:
+            msg += aux['title'] + '\n\n'
+
+        self.url_handler.send_message(msg, chat)
